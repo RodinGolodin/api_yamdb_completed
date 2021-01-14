189711267f391f957932de7540b05f27424558b8
Sergey Rodin <sergeyrodin.sr@gmail.com>
2021-01-14 23:00:06 +0700

    Added genres and titles

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Category, Comment, Review
+from .models import Category, Comment, Genre, Review, Title
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -19,7 +19,7 @@ class ReviewSerializer(serializers.ModelSerializer):
             request.method != 'PATCH' and
             Review.objects.filter(title=title, author=request.user).exists()
         ):
-            raise serializers.ValidationError('The score already exists')
+            raise serializers.ValidationError('Review already exists')
         return data
 
 
@@ -39,3 +39,29 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+        
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(read_only=True, many=True)
+    category = CategorySerializer(read_only=True)
+    rating = serializers.IntegerField(read_only=True, required=False)
+
+    class Meta:
+        fields = "__all__"
+        model = Title
+
+
+class TitleWriteSerializer(TitleReadSerializer):
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(), slug_field='slug', many=True
+    )
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(), slug_field='slug'
+    )
