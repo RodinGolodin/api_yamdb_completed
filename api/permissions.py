f4edcec5c815bfa3cc2fd872e2a650cf29ea0a8c
Sergey Rodin <sergeyrodin.sr@gmail.com>
2021-01-14 17:20:52 +0700

    YAM-11, YAM-12, YAM-13, YAM-14, YAM-15

--- api/permissions.py
+++ api/permissions.py
@@ -12,21 +12,21 @@ class IsAdminUserOrReadOnly(BasePermission):
         return request.method in SAFE_METHODS or request.user.is_superuser
 
 
-class CustomPermission(BasePermission):
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        if request.method in ['POST', 'PATCH', 'DELETE']:
-            return request.user.is_authenticated
+# class CustomPermission(BasePermission):
+#     def has_permission(self, request, view):
+#         if request.method in SAFE_METHODS:
+#             return True
+#         if request.method in ['POST', 'PATCH', 'DELETE']:
+#             return request.user.is_authenticated
 
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return (
-            obj.author == request.user
-            or request.user.is_staff
-            or request.user.is_superuser
-        )
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in SAFE_METHODS:
+#             return True
+#         return (
+#             obj.author == request.user
+#             or request.user.is_staff
+#             or request.user.is_superuser
+#         )
         
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
